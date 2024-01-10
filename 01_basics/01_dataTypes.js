19b2ace7d9a1a6967d7714efe86867909047e0ee
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-10 09:59:13 +0000

    DataTypes in JS

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // we are Using Node.js , Not browser
+console.log("Santosh");
+/*
+number => 2^53
+bigint
+string
+boolean
+null => standalone value
+undefined => value is not assigned
+symbol => uniquesness in react component
+object
+*/
+let name = "santosh";
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 13513;
+let accountname = "Santosh Pathak";
+var accountPass = "12345";
+accountCity = "Dehradun";
+let accountState;
+// accountId = 34535;
+// console.log(accountId);
+/*
+prefer not to use var - 
+because of issue  in block scope and functional scope
+*/
+console.table([accountId,accountPass,accountname,accountCity,accountState]);
