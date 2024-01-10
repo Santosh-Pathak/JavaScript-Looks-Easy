7a29c084d4da9d9641972fdf9987f7ab8ee68f7c
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-10 09:54:02 +0000

    conversion of data types

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = null;
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" =? NaN
+// true =? 1 : false =? 0
+
+// let isLoggedIn ="gvmh";
+// let booleanIsLoggesIn = Boolean(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log( booleanIsLoggesIn);
+// console.log(typeof booleanIsLoggesIn);
+
+// 1=?true
+// "" =?false
+// "anything" => true
+
+let  someNumber = 33;
+let stringNumber  = String(someNumber);
+console.log(someNumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
+
