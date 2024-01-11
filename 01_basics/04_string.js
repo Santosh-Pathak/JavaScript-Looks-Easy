956a0d7cb1745a905a203a6b97e82616d4419d45
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-11 13:59:30 +0000

    String Concept Completed

---
+++ 01_basics/04_string.js
@@ -0,0 +1,24 @@
+let name = "santosh";
+const repoCount = 50;
+
+// console.log(name+repoCount); // Bad Practice and outdated
+// String InterPolation => ``
+console.log(`Hello My Name  is ${name} and My RepoCount is ${repoCount}`);
+console.log(typeof name);
+
+let Name  = new String('Santosh');
+console.log(`Hello My Name  is ${Name} and My RepoCount is ${repoCount}`);
+console.log(typeof Name);
+
+// console.log(Name.__proto__);
+// console.log(Name.length);
+// console.log(Name.toUpperCase);
+// console.log(Name.charAt(3));
+// let substr = Name.substring(1,4);
+// console.log(substr);
+
+let url = "https://santosh Pathak.com";
+console.log(url.replace(' ','_'));
+
+console.log(url.includes("santosh5"));
+  
