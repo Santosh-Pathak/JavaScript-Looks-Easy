443e54944d476519a2dfc86fea14b7fed666ef2b
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-12 17:57:48 +0000

    Arrays Concept Completed

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+//Arrays
+// 2 ways to initilise Arrays
+const arr = [1,2,3,4];
+// const strArray = ["Santosh", "sunder","Pawan"];
+// const arr2 = new Array(1,2,3,4);
+// console.log(arr[0]);
+
+// ARRAY METHODS
+// arr.push(5); // insertat end
+// console.log(arr);
+// arr.unshift(7); //insert at front of array
+// console.log(arr);
+// arr.shift(); // delete form starting of the array
+// arr.shift();
+// arr.shift();
+// console.log(arr);
+// console.log(arr.includes(88));
+// console.log(arr.indexOf(88)); // -1 show element not present
+
+// const newArr = arr.join(); // change into string dataType
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// SLICE AND SPICE
+// console.log("A",arr);
+// const myNewArr1 = arr.slice(1,3);
+// console.log("A",myNewArr1);
+// console.log("A",arr);
+
+// console.log("A",arr);
+// const myNewArr2 = arr.splice(1,3);
+// console.log("A",myNewArr2);
+// console.log("A",arr);
+
+
+
+
+
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,22 @@
+const marvel = ["thor","IronMan","spiderMan"];
+const dc = ["superMan" , "flash", "batMan"];
+
+// marvel.push(dc);
+// console.log(marvel);
+
+// marvel.concat(dc);
+// console.log(marvel);
+
+// const marvel_dc = marvel.concat(dc); //merge two arrays
+// console.log(marvel_dc);
+
+// const allHeros = [...dc,...marvel]; //merge two arrays
+// console.log(allHeros);
+
+
+console.log(Array.isArray("santosh")); //check whether arrays or not
+console.log(Array.from("santosh")); // cpnvert to array
+
+
+
+
