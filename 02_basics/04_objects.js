4bb2a56521ec33cf6cac7056f1ab2ed25777e937
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-14 19:12:50 +0000

    Objects COmpleted

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -14,6 +14,23 @@ const regularUser ={
         }
     }
 }
-console.log(regularUser);
-console.log(regularUser.fullName.userFullName.firstName);
+// console.log(regularUser);
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {1:"santosh",2:"Sneha",3:"Sunder"};
+const obj2 = {4:"Pawan",5:"Dev",6:"Darpan"};
+
+// const obj3  = {obj1,obj2}; 
+//const obj = Object.assign({},obj1,obj2); //{}-> to combine all the parameter (for more than 2 objects , we have to use {} to combine all other )
+// console.log(obj);
+
+const obj3 = {...obj1,...obj2}; //mostly used
+// console.log(obj3);
+
+
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser)); // show all the keys
+const arrayOfKeys =  Object.keys(tinderUser); // an array of keys of Object TInderUser
+const arrayOfValues = Object.values(tinderUser);
 
