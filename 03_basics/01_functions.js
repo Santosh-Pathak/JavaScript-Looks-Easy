4d02dfff2b6c059639f58897eeb740f854d96cff
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-15 14:13:19 +0000

    Basics of Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,11 @@
+// function printNtime()
+// {
+//     console.log("Hello World");
+// }
+// printNtime();
+function add(n,m){
+    console.log(n+m);
+    return n*m;
+}
+const result = add(2,3);
+console.log(result);
