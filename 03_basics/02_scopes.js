ad211af327818f7c846218e297df29b85149c655
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-16 17:46:20 +0000

    Concept of Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+// let  a= 10;
+// const b = 20;
+// var c = 30;
+if(true)
+{
+let  a= 10;
+const b = 20;
+var c = 30;
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+// here var is also accessible outside the SCOPE . (problem of SCOPE)
