16dfec9f0f15ab9601c937d2a610078a1489cdf7
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-17 18:25:40 +0000

    Closure of Funtions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,5 +9,20 @@ var c = 30;
 }
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 // here var is also accessible outside the SCOPE . (problem of SCOPE)
+
+// Closure and SCope 
+function one()
+{
+    const userName = "Santosh"
+    function two()
+    {
+        const website  = "Youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+    // two()
+}
+one();
+//Closure =>in closure child Functions can use the variable of Parent Functions
