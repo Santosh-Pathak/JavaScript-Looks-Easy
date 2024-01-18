23aa244d43561ccdce149e0dad645a661bb17af0
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2024-01-18 18:06:45 +0000

    Immediately Invoked Function Expression(IIFE)

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+ // Immediately Invoked Function Expression
+( function gym(){ //named IIFE
+    console.log(`DB CONNECTED`);
+ })();
+
+ (  (name)=>{ 
+    console.log(`DB CONNECTED ${name}`);
+ })("Santosh");
+
